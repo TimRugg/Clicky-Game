91fe06fc1af3f37ab2bc434eba01c6a256d03614
timster2938 <ruggt001@icloud.com>
2018-04-21 16:55:18 -0400

    Image and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+An application built from scratch using Create React App. 
+
+1. Copy the repository.
+2. Go to git and clone to PC.
+3. In main Clicky-Game folder, perform a Yarn Install.
+4. Run game by using Yarn Start.
+
+![Clicky Game](/public/ClickyGameScreen.png)
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => 
+    <div class="jumbotron text-center">
+        <h1 class="display-4">Clicky Game!</h1>
+        <h3>Click on an image to earn points, but don't click on any more than once!</h3>
+    </div>;
+
+export default Title;
+
